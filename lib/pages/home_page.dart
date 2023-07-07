2cf1a92e30af99c22cf11796eeb28193063acbd8
mayconndouglass <mayconn.douglass1414@gmail.com>
2023-07-07 20:24:59 -0300

    Implemented genre search functionality

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,7 +4,6 @@ import 'package:movie_app/widgets/title_card.dart';
 import 'package:number_paginator/number_paginator.dart';
 import 'package:provider/provider.dart';
 
-
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -13,23 +12,21 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  String selectedGenre = '';
+  int currentPage = 1;
+  late List<dynamic> genreList;
+
   @override
   void initState() {
     super.initState();
-    Provider.of<TitleProvider>(context, listen: false).loadData();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      Provider.of<TitleProvider>(context, listen: false).loadData();
+      genreList = await Provider.of<TitleProvider>(context, listen: false).fetchGenres();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('Criado por Maycon Douglas'),
-          duration: Duration(seconds: 5),
-        ),
-      );
-    });
-
     return Consumer<TitleProvider>(
       builder: (context, provider, _) {
         return Scaffold(
@@ -44,31 +41,78 @@ class HomePageState extends State<HomePage> {
             elevation: 16.0,
             centerTitle: true,
             backgroundColor: Colors.black87,
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () {
+                  _openGenreDropdown(provider);
+                },
+              ),
+            ],
           ),
           body: Column(
             children: [
               Expanded(
                 child: RefreshIndicator(
                   onRefresh: () => provider.loadData(),
-                    child: ListView.builder(
-                      itemCount: provider.titleList.length,
-                      itemBuilder: (context, index) {
-                        final data = provider.titleList[index];
-                        return MovieCard(data: data);
-                      }
-                    ),
-                ), 
+                  child: ListView.builder(
+                    itemCount: provider.titleList.length,
+                    itemBuilder: (context, index) {
+                      final data = provider.titleList[index];
+                      return MovieCard(data: data);
+                    },
+                  ),
+                ),
               ),
               NumberPaginator(
                 numberPages: 20,
                 onPageChange: (page) {
-                  provider.loadData(page: page + 1);
+                  setState(() {
+                    currentPage = page;
+                  });
+
+                  provider.loadData(genre: selectedGenre, page: page + 1);
                 },
               )
-            ],           
+            ],
           ),
         );
       },
     );
   }
+
+  void _openGenreDropdown(provider) {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Selecione um gênero'),
+        content: DropdownButton<String>(
+          value: selectedGenre.isNotEmpty ? selectedGenre : null,
+          onChanged: (newValue) {
+            setState(() {
+              selectedGenre = newValue!;
+            });
+            provider.loadData(genre: selectedGenre, page: currentPage);
+            Navigator.pop(context);
+          },
+          items: [
+            const DropdownMenuItem<String>(
+              value: null,
+              child: Text('Selecionar gênero'),
+            ),
+            ...genreList.map((genre) {
+              return DropdownMenuItem<String>(
+                value: genre,
+                child: Text(genre),
+              );
+            }).toList(),
+          ],
+        ),
+      );
+    },
+  );
+}
+
+
 }
--- lib/providers/title_provider.dart
+++ lib/providers/title_provider.dart
@@ -8,8 +8,12 @@ class TitleProvider with ChangeNotifier {
   final _baseUrl = 'https://moviesdatabase.p.rapidapi.com/titles?year=2023&info=base_info';
   final List<TitleModel> titleList = [];
 
-  Future<void> loadData({int page = 1}) async {
-    final String url = '$_baseUrl&page=$page';
+  Future<void> loadData({int page = 1, String genre = ''}) async {
+    String url = '$_baseUrl&page=$page';
+
+    if (genre.isNotEmpty) {
+      url = '$url&genre=$genre';
+    }
 
     const headers = {
       'X-RapidAPI-Key': '2c60b4747bmsh8892a80c26977d0p19cd87jsnbffdef5116a0',
@@ -33,6 +37,28 @@ class TitleProvider with ChangeNotifier {
       titleList.addAll(results.map((data) => TitleModel.fromJson(data)));
       
       notifyListeners();
+    } else {
+      throw Exception('Ops, Erro no carregamento dos dados');
+    }
+  }
+
+  Future<List<dynamic>> fetchGenres() async {
+    const String url = 'https://moviesdatabase.p.rapidapi.com/titles/utils/genres';
+    
+    const headers = {
+      'X-RapidAPI-Key': '2c60b4747bmsh8892a80c26977d0p19cd87jsnbffdef5116a0',
+      'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+    };
+
+    final response = await http.get(Uri.parse(url), headers: headers);
+
+    if (response.statusCode == 200) {
+      dynamic body = jsonDecode(response.body);
+      List<dynamic> results = body['results'];
+      
+      results.removeAt(0);
+      
+      return results;
     } else {
       throw Exception('Ops, Erro no carregamento dos dados');
     }
